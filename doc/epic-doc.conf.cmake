12cfd5d4486136a4a10d02e7f1b08053af80dfc1
acastro <acastro@sodisa.com>
2019-03-11 13:05:51 -0600

    Adds epic-doc files

---
+++ doc/epic-doc.conf.cmake
@@ -0,0 +1,7 @@
+project-name: b2d
+input-directory: ${CMAKE_SOURCE_DIR}/include
+output-directory: ${CMAKE_SOURCE_DIR}/doc
+stylesheet-directory: ${CMAKE_SOURCE_DIR}/doc/css
+header-filename: ${CMAKE_SOURCE_DIR}/doc/nav.html
+footer-filename: ${CMAKE_SOURCE_DIR}/doc/footer.html
+md-directory: ${CMAKE_SOURCE_DIR}/doc/md
---
+++ doc/md/b2d.md
@@ -0,0 +1,7 @@
+# B2D
+
+2d game framework for C++
+
+## Author
+
+Alberto Castro Estrada
